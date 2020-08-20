38dc37eca743c772b3f519bc6574b6aec61a25b7
Cafolkes <carl.axel.folkestad@gmail.com>
2020-08-20 19:24:01 +0200

    Added Lipschitz calculation and max residual calculation

--- dubin_example.m
+++ dubin_example.m
@@ -11,7 +11,7 @@ global Ts vm rm am x_bdry
 affine_dynamics = @(x) dubin(x);                    % System dynamics, returns [f,g] with x_dot = f(x) + g(x)u
                                                     % State is defined as x = [X,Y,v,theta], u = [a,r]
 sim_dynamics = @(x,u) dubin_sim(x,u);               % System dynamics used for simulation
-sim_process = @(x,ts) dubin_sim_process(x,ts);            % Processing of states while simulating
+sim_process = @(x,ts) dubin_sim_process(x,ts);      % Processing of states while simulating
 Ts = 0.1;                                           % Sampling interval
 vm = 4;                                             % Maximum velocity
 rm = 1.2;                                           % Maximum yaw rate
@@ -24,10 +24,10 @@ stop_crit1 = @(t,x)(abs(x(3))<=0);                  % Stop if velocity is zero
 %Koopman learning parameters:
 dubin_dictionary;                                   % Generate dictionary for Dubin's car system
 func_dict = @(x) dubin_D(x(1),x(2),x(3),x(4));      % Function dictionary, returns [D,J] = [dictionary, jacobian of dictionary]
-n_samples = 100;                                    % Number of initial conditions to sample for training
+n_samples = 10;                                     % Number of initial conditions to sample for training
 
 %Collision avoidance experiment parameters:
-global T_exp alpha 
+global T_exp alpha obs r
 x0 = [0;0;vm;0];                                    % Initial condition for experiment
 x_des = [10;0;0;0];                                 % Desired state for legacy controller
 mpc_horizon = 20;                                   % Time horizon of legacy controller
@@ -35,8 +35,8 @@ legacy_controller = @(x) MIQP_MPC_v3(x,x_des,20);   % Legacy controller (MPC)
 options = qpOASES_options('printLevel',0);          % Solver options for supervisory controller
 T_exp = 10;                                         % Experiment length
 alpha = 1;                                          % CBF strengthening parameter
-r = 1;                                              % Radius of circular obstacle
 obs = [5;0];                                        % Center of circular obstacle
+r = 1;                                              % Radius of circular obstacle
 barrier_func = @(x) round_obs(x,obs,r);             % Barrier function
 
 %% Learn approximated discrete-time Koopman operator:
@@ -45,9 +45,11 @@ X_train = collect_data(sim_dynamics, sim_process, con1, stop_crit1, n_samples);
 [K, C] = edmd(X_train, func_dict);
 K_pows = precalc_matrix_powers(N_max,K);
 
+L = calc_lipschitz(4,2, affine_dynamics, con1);
+e_max = calc_max_residual(X_train, func_dict, K, C);
+%error_bound = @(k,x)
+%koopman_error_bound(t,x,L,e_max,tt,K_pows,C,func_dict) (TODO)
 %Calculate error bound (TODO)
-    %-Calculate Lipschitz constant
-    %-Calculate maximum residual
 %Plot results (TODO)
     %-Plot data distribution
     %-Plot training prediction error and residuals
